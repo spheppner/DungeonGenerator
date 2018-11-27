bcdf4e9e54188fba4d5fc0c5b63b2a7d55e495c0
Simon Heppner <hasibandi@gmx.at>
2018-11-27 17:11:19 +0100

    Update DungeonGenerator.py

--- DungeonGenerator.py
+++ DungeonGenerator.py
@@ -9,14 +9,16 @@ legend = {".": {"name": "floor",
           "d": {"name": "door",
                 "prob": None},
           "<": {"name": "stair down",
-                "prob": 0.03},
+                "prob": 0.02},
           ">": {"name": "stair up",
-                "prob": 0.03}
-         }
-
+                "prob": 0.02}
+         } 
+         
 rooms = []
 d = []
 ft = 1
+et = 1
+rt = 1
 maxlines = 18
 maxchars = 70
 maxrooms = 4
@@ -60,7 +62,15 @@ for x in range(1, maxchars-1):
             for _ in range(howmuch):
                 y -= 1
                 d[y][x] = "."
-    # ---- new room? and stair? ----
+    # ---- new room? ----
+    elif rt == 1:
+        rt = 0
+        room_nr += 1
+        maxbreite = maxchars-2 - x
+        b = min(random.randint(3, 15), maxbreite)
+        maxtiefe = maxlines-2 - y
+        h = min(random.randint(2, 10), maxtiefe)
+        rooms.append((x, y, b, h))
     elif random.random() < 0.1 and room_nr < maxrooms:
         room_nr += 1
         maxbreite = maxchars-2 - x
@@ -88,26 +98,51 @@ for r in rooms:
     for y, line in enumerate(d):
         for x, char in enumerate(line):
             if y >= y1 and y <= y1+h and x >= x1 and x <= x1+ b:
-                d[y][x] = "."
-                if ft == 1:
-                    ft = 0
-                    d[y][x] = "<"
-                elif random.random() < stairdown_prob and stair_nr < maxstairs:
-                    stair_nr += 1
-                    d[y][x] = "<"
-                elif ft == 1:
-                    ft = 0
-                    d[y][x] = ">"
-                elif random.random() < stairup_prob and stair_nr < maxstairs:
-                    stair_nr += 1
-                    d[y][x] = ">"
-                elif random.random() < rock_prob:
-                    if rock_nr < maxrocks and y > y1+1 and y < y1+ h-2 and x > x1+1 and x < x1+b-2:
+                d[y][x] = "."     
+#                if ft == 1:
+#                    ft = 0
+#                    d[y][x] = "<"     
+#                elif random.random() < stairdown_prob and stair_nr < maxstairs:
+#                    stair_nr += 1
+#                    d[y][x] = "<"
+#                elif et == 1:
+#                    et = 0
+#                    d[y][x] = ">"
+#                elif random.random() < stairup_prob and stair_nr < maxstairs:
+#                    stair_nr += 1
+#                    d[y][x] = ">"
+                if random.random() < rock_prob:
+                    if rock_nr < maxrocks and y > y1 and y < y1+ h and x > x1 and x < x1+b:
                         rock_nr += 1
                         d[y][x] = "#"
                  
+# treppenschleife
 
-    
+pebbles = []
+for y, line in enumerate(d):
+    for x, char in enumerate(line):
+        if d[y][x] == ".":
+            pebbles.append((x,y))
+
+random.shuffle(pebbles)
+max_pebbles = len(pebbles)
+n = 0
+x = pebbles[n][0]
+y = pebbles[n][1]
+d[y][x] = "<"
+n = 1
+x = pebbles[n][0]
+y = pebbles[n][1]
+d[y][x] = ">"
+for n in range(2, random.randint(1, max_pebbles)):
+    x = pebbles[n][0]
+    y = pebbles[n][1]
+    if random.random() < stairdown_prob:
+        d[y][x] = "<"
+    elif random.random() < stairup_prob:
+        d[y][x] = ">"
+
+
 # ---- dungeon printer ----
 
 for l in d:
@@ -115,3 +150,5 @@ for l in d:
         print(char, end="")
     print()    
 #print(rooms)
+
+print(max_pebbles)
